e43d81a381db142c52c192b916b172aa3f7a9eb5
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-11-08 21:50:58 +0200

    add graphql setting

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     "graphene_django" ,
 
     'book'
 ]
@@ -133,3 +134,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "book.schema.schema"
+}
--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from graphene_django.views import GraphQLView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
